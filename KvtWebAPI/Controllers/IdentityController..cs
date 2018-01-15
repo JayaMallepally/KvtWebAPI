39317fca41cd53ef2177663cf6c6c6581a161231
JayaMallepally <jaya@kassavirtanen.fi>
2018-01-15 12:35:41 +0200

    Created Api to test the authentication

---
+++ KvtWebAPI/Controllers/IdentityController..cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+        [Route("identity")]
+        [Authorize]
+        public class IdentityController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult Get()
+            {
+            //return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+            return new JsonResult("hello");
+
+        }
+        }
+
+   
+    
+}
